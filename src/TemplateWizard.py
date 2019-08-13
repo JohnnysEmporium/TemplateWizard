473bfb6cf5e8318732793d8779157dced95db915
Jan Sobczak <sobczak.yzf@gmail.com>
2019-08-13 11:12:01 +0200

    misc

--- src/TemplateWizard.py
+++ src/TemplateWizard.py
@@ -2,23 +2,25 @@ import intestines
 
 while True:
     a = input('''
-    Select Number
-    1. Create New                                    --> working on template.docx
-    2. Change Color of existing one or add update    --> working on color_change.docx
-    3. Exit
-    ---
-    ''')
+Select Number
+---------------
+1. Create New                                    --> working on template.docx
+2. Change Color of existing one or add update    --> working on color_change.docx
+3. Exit
+---------------
+''')
     if a == '1':
         intestines.paster()
     elif a == '2':
         color = input('''
-        Select Color
-        1. Red
-        2. Orange
-        3. Green
-        4. Add latest update to Notification
-        ---
-        ''')
+Select Color
+---------------
+1. Red
+2. Orange
+3. Green
+4. Add latest update to Notification
+---------------
+''')
         print(color)
         if color != '1' and color != '2' and color != '3' and color != '4':
             print('\n!!!---You must input a number between 1 and 3---!!!\n')
